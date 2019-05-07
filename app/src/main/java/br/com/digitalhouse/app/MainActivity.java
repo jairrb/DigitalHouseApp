c401f9c0db84b084d11508318bd7aca3ba383c9a
jairrb <jairrb@live.com>
2019-05-06 22:11:28 -0300

    Android Aula 2

--- app/src/main/java/br/com/digitalhouse/app/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/app/MainActivity.java
@@ -2,8 +2,10 @@ package br.com.digitalhouse.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText editTextEmail;
@@ -19,5 +21,25 @@ public class MainActivity extends AppCompatActivity {
         editTextSenha = findViewById(R.id.editTextSenha);
         btnLogin = findViewById(R.id.btnLogin);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString();
+                String senha = editTextSenha.getText().toString();
+
+                if (email.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "E-mail não pode ser vazio!", Toast.LENGTH_SHORT).show();
+                }else if (!email.contains("@")) {
+                    Toast.makeText(MainActivity.this, "E-mail invalido!", Toast.LENGTH_SHORT).show();
+                } else if(senha.length() < 6){
+                    Toast.makeText(MainActivity.this, "Senha deve ter no minimo 6 caracteres", Toast.LENGTH_SHORT).show();
+                } else if(senha.equals("123456")) {
+                    Toast.makeText(MainActivity.this, "Senha padrão Ok", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
+
+
 }
